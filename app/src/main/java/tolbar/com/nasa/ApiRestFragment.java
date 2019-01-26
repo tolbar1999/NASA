67fc1b5351f5a8cc18311e3fbd497a49356859ed
TOLBAR <tolbar1999@gmail.com>
2019-01-26 00:38:06 -0500

    Se agregó el nav API REST

---
+++ app/src/main/java/tolbar/com/nasa/ApiRestFragment.java
@@ -0,0 +1,30 @@
+package tolbar.com.nasa;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ApiRestFragment extends Fragment {
+
+
+    public ApiRestFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        getActivity().setTitle("API REST");
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_api_rest, container, false);
+    }
+
+}

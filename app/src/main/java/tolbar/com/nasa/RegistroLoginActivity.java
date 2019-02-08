08c75bf5f9de7807360ec99c20e90a65f31fdaea
TOLBAR <tolbar1999@gmail.com>
2019-02-07 21:09:56 -0500

    Para dejar guardado finalmente y le asigne id a los componentes

--- app/src/main/java/tolbar/com/nasa/RegistroLoginActivity.java
+++ app/src/main/java/tolbar/com/nasa/RegistroLoginActivity.java
@@ -2,12 +2,29 @@ package tolbar.com.nasa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class RegistroLoginActivity extends AppCompatActivity {
+
+    private EditText txtNombreRegistro;
+    private EditText txtApellidoRegistro;
+    private EditText txtCelularRegistro;
+    private EditText txtCorreoRegistro;
+    private EditText txtClaveRegistro;
+    private CheckBox cbAceptoTerminosRegistro;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_login);
+
+        txtNombreRegistro = findViewById(R.id.txtNombreRegistro);
+        txtApellidoRegistro = findViewById(R.id.txtApellidoRegistro);
+        txtCelularRegistro = findViewById(R.id.txtCelularRegistro);
+        txtCorreoRegistro = findViewById(R.id.txtCorreoRegistro);
+        txtClaveRegistro = findViewById(R.id.txtClaveRegistro);
+        cbAceptoTerminosRegistro = findViewById(R.id.cbAceptoTerminosRegistro);
     }
 }

94c5fa451816f860eed0cf991b6f996a60ca9a17
TOLBAR <tolbar1999@gmail.com>
2019-02-07 23:44:52 -0500

    Se agregó la dependencia de *butterknife* para las anotaciones de @BindView y se mapearon los atributos de ejemplo para el registro login

--- app/src/main/java/tolbar/com/nasa/RegistroLoginActivity.java
+++ app/src/main/java/tolbar/com/nasa/RegistroLoginActivity.java
@@ -2,17 +2,33 @@ package tolbar.com.nasa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class RegistroLoginActivity extends AppCompatActivity {
 
-    private EditText txtNombreRegistro;
-    private EditText txtApellidoRegistro;
-    private EditText txtCelularRegistro;
-    private EditText txtCorreoRegistro;
-    private EditText txtClaveRegistro;
-    private CheckBox cbAceptoTerminosRegistro;
+    @BindView(R.id.txtNombreRegistro)
+    EditText txtNombreRegistro;
+
+    @BindView(R.id.txtApellidoRegistro)
+    EditText txtApellidoRegistro;
+
+    @BindView(R.id.txtCelularRegistro)
+    EditText txtCelularRegistro;
+
+    @BindView(R.id.txtCorreoRegistro)
+    EditText txtCorreoRegistro;
+
+    @BindView(R.id.txtClaveRegistro)
+    EditText txtClaveRegistro;
+
+    @BindView(R.id.cbAceptoTerminosRegistro)
+    CheckBox cbAceptoTerminosRegistro;
 
 
     @Override
@@ -20,11 +36,10 @@ public class RegistroLoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_login);
 
-        txtNombreRegistro = findViewById(R.id.txtNombreRegistro);
-        txtApellidoRegistro = findViewById(R.id.txtApellidoRegistro);
-        txtCelularRegistro = findViewById(R.id.txtCelularRegistro);
-        txtCorreoRegistro = findViewById(R.id.txtCorreoRegistro);
-        txtClaveRegistro = findViewById(R.id.txtClaveRegistro);
-        cbAceptoTerminosRegistro = findViewById(R.id.cbAceptoTerminosRegistro);
+        ButterKnife.bind(this);
+    }
+
+    public void verAhora(View view) {
+        Toast.makeText(this, txtNombreRegistro.getText().toString(), Toast.LENGTH_SHORT).show();
     }
 }
